3eadcb363150f6a1dda5ebf0e9013ceca4618579
Ashish473797 <ashish473797@gmail.com>
2024-02-13 23:22:46 +0530

    loading animation is done

---
+++ port/animation.js
@@ -0,0 +1,46 @@
+let tl = gsap.timeline();
+tl.to('#loader', {
+    delay: 1,
+    duration: 5,
+    left: "0%",
+});
+
+tl.to('#upperLoader', {
+    delay: 1,
+    duration: 1,
+    top: "-50%",
+    borderRadius: "24%",
+}, 'loader')
+.to('#lowerLoader', {
+    delay: 1,
+    duration: 1,
+    bottom: "-50%",
+    borderRadius: "24%",
+}, 'loader')
+.to('#loader', {
+    delay: "1",
+    left: "500%",
+}, 'loader')
+.to('#upperText', {
+    delay: 1.2,
+    duration: 2,
+    bottom: "-50%",
+}, 'loader')
+.to('#lowerText', {
+    delay: 1.2,
+    duration: 2,
+    bottom: "-50%",
+}, 'loader')
+.to('main', {
+    duration: 2,
+    top: "0",
+}, 'loader')
+.to('section', {
+    display: "block",
+}, 8)
+
+
+
+
+
+
