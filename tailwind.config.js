1522e4b071eb5d17c2cdadd935a485b7d78cdea2
Ashish473797 <ashish473797@gmail.com>
2024-02-14 13:34:11 +0530

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'Eerie black': '#222222',
+        'White': '#FFFFFF',
+        'Lapis Lazuli': '#1C5D99',
+        'Moonstone': '#639FAB',
+        'Powder blue': '#BBCDE5',
+      },
+      fontFamily: {
+        palanquin: ['Palanquin', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+      backgroundImage: {
+        'profile': 'url()'
+      }
+    },
   },
   plugins: [],
 }
