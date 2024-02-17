7148be9b9668810e1939647a6a7b68e842ea724f
Ashish473797 <ashish473797@gmail.com>
2024-02-17 20:17:12 +0530

    updated

--- port/animation.js
+++ port/animation.js
@@ -6,3 +6,38 @@ document.addEventListener('mousemove', (currPosition) => {
         duration: 0.5,
     })
 });
+
+let tl = gsap.timeline();
+
+tl.to('#loader', {
+    delay: 1,
+    duration: 4,
+    left: "0%",
+})
+.to('#upperLoader', {
+    delay: 1,
+    duration: 1,
+    top: "-51%",
+}, 'loader')
+.to('#lowerLoader', {
+    delay: 1,
+    duration: 1,
+    bottom: "-51%",
+}, 'loader')
+.to('#loader', {
+    delay: "1",
+    left: "500%",
+}, 'loader')
+.to('#upperText', {
+    delay: 1.2,
+    duration: 2,
+    bottom: "-50%",
+}, 'loader')
+.to('#lowerText', {
+    delay: 1.2,
+    duration: 2,
+    bottom: "-50%",
+}, 'loader')
+.to('section', {
+    display: "block",
+}, 7);
