78f4ae70b0fcc19414ed3ff957abaec822a1b688
Ashish473797 <ashish473797@gmail.com>
2024-02-18 16:51:51 +0530

    updated

--- port/animation.js
+++ port/animation.js
@@ -1,11 +1,62 @@
 
 document.addEventListener('mousemove', (currPosition) => {
-    gsap.to('.myCursor', {
+    gsap.to('#myCursor', {
         x: currPosition.x,
         y: currPosition.y,
         duration: 0.5,
     })
 });
+
+let li = document.querySelectorAll('.menuList');
+
+li.forEach(element => {
+    element.addEventListener('mouseover', (e) => {
+        gsap.to('#myCursor', {
+            scale: 1.8,
+        })
+    })
+});
+
+li.forEach(element => {
+    element.addEventListener('mouseleave', (e) => {
+        gsap.to('#myCursor', {
+            scale: 1,
+        })
+    })
+});
+
+
+let icons = document.querySelectorAll('i');
+
+icons.forEach(element => {
+    element.addEventListener('mouseover', (e) => {
+        gsap.to('#myCursor', {
+            scale: 1.8,
+        })
+    })
+});
+
+icons.forEach(element => {
+    element.addEventListener('mouseleave', (e) => {
+        gsap.to('#myCursor', {
+            scale: 1,
+        })
+    })
+});
+
+let logo = document.querySelector('.logo');
+
+logo.addEventListener('mouseover', (e) => {
+    gsap.to('#myCursor', {
+        scale: 2,
+    })
+})
+
+logo.addEventListener('mouseleave', (e) => {
+    gsap.to('#myCursor', {
+        scale: 1,
+    })
+})
 
 let tl = gsap.timeline();
 
@@ -18,26 +69,33 @@ tl.to('#loader', {
     delay: 1,
     duration: 1,
     top: "-51%",
-}, 'loader')
+}, 'loading')
 .to('#lowerLoader', {
     delay: 1,
     duration: 1,
     bottom: "-51%",
-}, 'loader')
+}, 'loading')
 .to('#loader', {
     delay: "1",
     left: "500%",
-}, 'loader')
+}, 'loading')
 .to('#upperText', {
     delay: 1.2,
     duration: 2,
     bottom: "-50%",
-}, 'loader')
+}, 'loading')
 .to('#lowerText', {
     delay: 1.2,
     duration: 2,
     bottom: "-50%",
-}, 'loader')
+}, 'loading')
+.from('header', {
+    duration: 2,
+    top: '100vh',
+}, 'loading')
 .to('section', {
     display: "block",
+}, 7)
+.to('#myLoader', {
+    display: 'none',
 }, 7);
